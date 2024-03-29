7947ea5a5f684329555c080f39ec1854f3b1cd5e
RYanIndieDev <ryanxukeepwalking@gmail.com>
2024-03-29 10:59:17 -0400

    update test files

--- files/js/index.js
+++ files/js/index.js
@@ -5,6 +5,8 @@ const TUTORIALS_CONTENT_CONTAINER = document.getElementById('tutorials-content-c
 const BLOGS_CONTENT_CONTAINER = document.getElementById('blogs-content-container');
 const CONTACT_CONTENT_CONTAINER = document.getElementById('contact-content-container');
 
+let tutorialArticleContainer;
+
 initializeContent();
 initializeTheme();
 initializeSidebarStyle();
@@ -13,6 +15,118 @@ initializeDisplayModeToggle();
 initializeNaviButtons();
 initializeIconLinks();
 SetCurrentYear();
+
+function initializeContent()
+{
+    let homeHTML;
+    let gamesHTML;
+    let tutorialsHTML;
+    let blogsHTML;
+    let contactHTML;
+
+    Promise.all([
+        getHtmlContent('files/html/home.html'),
+        getHtmlContent('files/html/games.html'),
+        getHtmlContent('files/html/tutorials.html'),
+        getHtmlContent('files/html/blogList.html'),
+        getHtmlContent('files/html/contact.html')])
+        .then(contents =>
+        {
+            [homeHTML, gamesHTML, tutorialsHTML, blogsHTML, contactHTML] = contents;
+
+            loadContent(HOME_CONTENT_CONTAINER, homeHTML);
+            loadContent(GAMES_CONTENT_CONTAINER, gamesHTML);
+            loadContent(TUTORIALS_CONTENT_CONTAINER, tutorialsHTML);
+            loadContent(BLOGS_CONTENT_CONTAINER, blogsHTML);
+            loadContent(CONTACT_CONTENT_CONTAINER, contactHTML);
+
+            initializeGameWindow();
+
+            HOME_CONTENT_CONTAINER.style.display = "flex";
+
+            tutorialArticleContainer = document.getElementById('tutorial-article-container');
+            testTutorialContainer();
+
+        })
+        .catch(error =>
+        {
+            console.error('Error initializing content:', error);
+        });
+
+    function getHtmlContent(path)
+    {
+        return fetch(path)
+            .then(response => response.text())
+            .then(htmlContent =>
+            {
+                return htmlContent;
+            });
+    }
+
+    function loadContent(contentContainer, html)
+    {
+        const content = document.createElement('div');
+        content.innerHTML = html;
+        content.style.flex = "1";
+        contentContainer.appendChild(content);
+        contentContainer.style.display = "none";
+    }
+
+    function initializeGameWindow()
+    {
+        const gameList = document.getElementById("game-list");
+        const gameWindow = document.getElementById('game-window');
+        const gameWindowCloseButton = document.getElementById('game-window-close-button');
+        const gameFrame = document.getElementById('game-frame');
+
+        gameWindowCloseButton.addEventListener("click", function ()
+        {
+            unloadGame();
+            gameWindow.style.display = "none";
+            gameList.style.display = "flex";
+        });
+
+        gameList.querySelectorAll('.game-panel').forEach(gamePanel =>
+        {
+            gamePanel.addEventListener("click", function ()
+            {
+                gameWindow.style.display = "flex";
+                gameList.style.display = "none";
+
+                switch (gamePanel.id)
+                {
+                    case 'game-panel-shooting-star':
+                        loadGame("https://itch.io/embed-upload/10046807?color=ffffff");
+                        break;
+                    case 'game-panel-02':
+                        break;
+                    case 'game-panel-03':
+                        break;
+                    default:
+                        break;
+                }
+            })
+        })
+
+        function loadGame(path)
+        {
+            unloadGame();
+            gameFrame.src = path;
+            gameFrame.setAttribute('allowfullscreen', '');
+            gameWindow.appendChild(gameFrame);
+        }
+
+        function  unloadGame()
+        {
+            const iframe = gameWindow.querySelector('iframe');
+
+            if (iframe)
+            {
+                gameWindow.removeChild(iframe);
+            }
+        }
+    }
+}
 
 function initializeTheme()
 {
@@ -129,115 +243,6 @@ function sidebarSearch()
 
 }
 
-function initializeContent()
-{
-    let homeHTML;
-    let gamesHTML;
-    let tutorialsHTML;
-    let blogsHTML;
-    let contactHTML;
-    
-    Promise.all([
-        getHtmlContent('files/html/home.html'),
-        getHtmlContent('files/html/games.html'),
-        getHtmlContent('files/html/tutorials.html'),
-        getHtmlContent('files/html/blogList.html'),
-        getHtmlContent('files/html/contact.html')])
-        .then(contents => 
-        {
-            [homeHTML, gamesHTML, tutorialsHTML, blogsHTML, contactHTML] = contents;
-            
-            loadContent(HOME_CONTENT_CONTAINER, homeHTML);
-            loadContent(GAMES_CONTENT_CONTAINER, gamesHTML);
-            loadContent(TUTORIALS_CONTENT_CONTAINER, tutorialsHTML);
-            loadContent(BLOGS_CONTENT_CONTAINER, blogsHTML);
-            loadContent(CONTACT_CONTENT_CONTAINER, contactHTML);
-            
-            initializeGameWindow();
-            
-            HOME_CONTENT_CONTAINER.style.display = "flex";
-            
-        })
-        .catch(error => 
-        {
-            console.error('Error initializing content:', error);
-        });
-
-    function getHtmlContent(path)
-    {
-        return fetch(path)
-            .then(response => response.text())
-            .then(htmlContent =>
-            {
-                return htmlContent;
-            });
-    }
-
-    function loadContent(contentContainer, html)
-    {
-        const content = document.createElement('div');
-        content.innerHTML = html;
-        content.style.flex = "1";
-        contentContainer.appendChild(content);
-        contentContainer.style.display = "none";
-    }
-
-    function initializeGameWindow()
-    {
-        const gameList = document.getElementById("game-list");
-        const gameWindow = document.getElementById('game-window');
-        const gameWindowCloseButton = document.getElementById('game-window-close-button');
-        const gameFrame = document.getElementById('game-frame');
-
-        gameWindowCloseButton.addEventListener("click", function ()
-        {
-            unloadGame();
-            gameWindow.style.display = "none";
-            gameList.style.display = "flex";
-        });
-        
-        gameList.querySelectorAll('.game-panel').forEach(gamePanel =>
-        {
-            gamePanel.addEventListener("click", function ()
-            {
-                gameWindow.style.display = "flex";
-                gameList.style.display = "none";
-                
-                switch (gamePanel.id)
-                {
-                    case 'game-panel-shooting-star':
-                        loadGame("https://itch.io/embed-upload/10046807?color=ffffff");
-                        break;
-                    case 'game-panel-02':
-                        break;
-                    case 'game-panel-03':
-                        break;
-                    default:
-                        break;
-                }
-            })
-        })
-        
-        function loadGame(path) 
-        {
-            unloadGame();
-            gameFrame.src = path;
-            gameFrame.setAttribute('allowfullscreen', '');
-            gameWindow.appendChild(gameFrame);
-        }
-        
-        function  unloadGame()
-        {
-            const iframe = gameWindow.querySelector('iframe');
-            
-            if (iframe)
-            {
-                gameWindow.removeChild(iframe);
-            }
-        }
-    }
-}
-
 function initializeNaviButtons()
 {
     let focusedNaviButton = document.getElementById("navi-home");
@@ -275,4 +280,84 @@ function initializeNaviButtons()
             shownContentContainer.style.display = "flex";
         });
     });
+
+    document.getElementById("navi-home").classList.add('navi-button-focused');
+    document.getElementById("navi-home").querySelectorAll('span').forEach(element =>
+    {
+        element.classList.add('focused');
+    })
 }
+
+function convertMarkDown(contentContainer, path)
+{
+    const converter = new showdown.Converter();
+
+    fetch(path)
+        .then(response => response.text())
+        .then(markdownText => 
+        {
+            contentContainer.innerHTML = converter.makeHtml(markdownText);
+        })
+        .catch(error => 
+        {
+            console.error('Error fetching Markdown file:', error);
+        });
+}
+
+function testTutorialContainer()
+{
+    convertMarkDown(tutorialArticleContainer, "files/md/GameDevLogs/shooting-star/shooting-star-update-v0.3.1.md");
+}
+
+function getMarkdownArticles(directoryPath) 
+{
+    const markdownFilesMap = new Map();
+    return fetchDirectoryContents(directoryPath)
+        .then(files => 
+        {
+            files.forEach(file => 
+            {
+                const fileName = extractFileName(file.path);
+                
+                if (fileName.endsWith('.md')) 
+                {
+                    const articleName = removeExtension(fileName);
+                    markdownFilesMap.set(articleName, file.path);
+                }
+            });
+            
+            return markdownFilesMap;
+        });
+    
+    function fetchDirectoryContents(directoryPath)
+    {
+        const apiURL = "https://api.github.com/repos/RYanXuDev/RYanXuDev.github.io/contents/" + directoryPath;
+        return fetch(apiURL)
+            .then(response => response.json())
+            .then(data => data.filter(item => item.type === 'file'));
+    }
+    
+    function extractFileName(filePath) 
+    {
+        return filePath.split('/').pop();
+    }
+    
+    function removeExtension(fileName) 
+    {
+        return fileName.split('.').slice(0, -1).join('.');
+    }
+}
+
+const directoryPath = 'files/md';
+getMarkdownArticles(directoryPath)
+    .then(markdownFilesMap => 
+    {
+        markdownFilesMap.forEach((path, name) => 
+        {
+            console.log(`Article Name: ${name}, Path: ${path}`);
+        });
+    })
+    .catch(error => 
+    {
+        console.error('Error fetching Markdown files:', error);
+    });
---
+++ files/md/GameDevLogs/shooting-star/shooting-star-update-v0.3.1.md
@@ -0,0 +1,25 @@
+# Shooting Star V0.3.1 Update
+
+这次更新包括了UI的全面重制，引入了在线排行榜功能和游戏设置保存功能。玩家现在可以在游戏结束后查看排行榜信息，并享受到更加个性化的游戏设置体验。此外，游戏玩法方面也有改进，包括新增的超载系统和能量值恢复机制的调整。
+
+  - UI重制：
+    - 除了角色的浮动血条，其他UI全部使用Unity UI Toolkit进行了重制。
+  - 功能添加：
+    - 在线排行榜功能。现在玩家在“生存模式(Survival Mode)”的最终得分会上传到网络并在游戏结束之后的计分画面显示玩家得分排行榜信息。玩家可看到自己的得分在排行榜上的排名，得分前100名的玩家将进入高分排行榜。
+    - 游戏设置保存功能。玩家对游戏的设置将保存在本地，下一次开始游戏时将会自动应用已保存的游戏设置。
+  - 玩法改进： 
+    - 新增玩家的超载值(Over Drive)。当玩家的子弹命中敌人机体时玩家将会积累超载值，当玩家的超载值达到100%时可按下相应按键进入超载状态，再次按下该按键则可以随时取消超载状态。
+    - 原有的能量值(Energy)修改为未满时自动缓慢恢复。和以前一样，当使用能量技能（暂时只有翻滚闪避技能，未来计划添加新技能）时将会消耗能量。
+
+----
+
+This update includes a complete UI overhaul, introducing online leaderboard functionality and game settings saving feature. Players can now view leaderboard information after the game ends and enjoy a more personalized gaming experience with saved settings. Additionally, gameplay has been improved with the addition of the Over Drive system and adjustments to the energy value recovery mechanism.
+
+  - UI Remake:
+    - With the exception of the character's floating health bar, all other UI elements have been revamped using the Unity UI Toolkit.
+  - Feature Additions:
+    - Online Leaderboard functionality: Player's final scores in "Survival Mode" are now uploaded to the network and displayed on the scoring screen after game over, showing player score ranking information. Players can see their score's position on the leaderboard, and the top 100 players will enter the high score leaderboard.
+    - Game Settings Save feature: Player's game settings will be saved locally, and will automatically apply the saved game settings the next time the game is started.
+  - Gameplay Enhancements:
+    - Introduction of Player Over Drive value: When player's projectiles hit enemy ships, the player will accumulate Over Drive value. When the Over Drive value reaches 100%, the player can press the corresponding button to enter Over Drive mode, and pressing the button again will cancel Over Drive mode at any time.
+    - Modification of existing Energy value to slowly regenerate when not full. Similar to before, using energy skills (currently only the roll-dodge skill, with plans to add new skills in the future) will deplete energy.
